b6eb29718daf858726aa448be47fb0118ad34e65
Ikillnukes <ikillnukes1@gmail.com>
2019-01-29 02:53:00 +0100

    Added MenuPage SetOptions + throw ArgumentException

--- EasyConsole/MenuPage.cs
+++ EasyConsole/MenuPage.cs
@@ -1,13 +1,28 @@
-﻿namespace EasyConsole
+﻿using System;
+
+namespace EasyConsole
 {
     public abstract class MenuPage : Page
     {
         protected Menu Menu { get; set; }
 
+        //public MenuPage(string title, Program program)
+        //    : this(title, program, null)
+        //{
+        //}
+
         public MenuPage(string title, Program program, params Option[] options)
             : base(title, program)
         {
             Menu = new Menu();
+
+            SetOptions(options);
+        }
+
+        public void SetOptions(params Option[] options)
+        {
+            if (options == null)
+                throw new ArgumentException("options");
 
             foreach (var option in options)
                 Menu.Add(option);
